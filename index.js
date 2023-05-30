8ad49548a88c6ed41d69f6b5e3ab179278cc3d9e
NIHAD O T <mernnihadsoft@gmail.com>
2023-05-30 09:33:12 +0530

    chat demo deploe

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import http from 'http'
+const app = express()
+const port = 4001
+
+const server = http.createServer(app)
+import { Server } from 'socket.io'
+const io = new Server(server)
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+
+    res.sendFile('/index.html')
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+    // console.log(socket.id);
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    })
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        socket.broadcast.emit('chat message', msg);
+    });
+    // io.emit('msg');
+    // io.on('connection', (socket) => {
+    //     socket.broadcast.emit('hi');
+    //   });
+
+
+})
+
+// io.on('connection', (socket) => {
+
+// });
+
+
+server.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
